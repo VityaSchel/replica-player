782521c70859d5a7f6aed976281b5a615bd6fb6d
Viktor <vityaschel@utidteam.com>
2022-07-14 15:49:06 +0400

    Build is finally working

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,34 @@
-import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import dts from "rollup-plugin-dts"
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import external from 'rollup-plugin-peer-deps-external'
 
-const packageJson = require("./package.json")
+const packageJson = require('./package.json')
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
+        format: 'cjs'
       },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
+        format: 'esm'
       },
     ],
     plugins: [
+      external(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
+      typescript({ tsconfig: './tsconfig.json' }),
+    ]
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()]
   },
 ];
