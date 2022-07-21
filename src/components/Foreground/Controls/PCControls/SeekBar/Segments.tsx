740c27bad00c5bf1f7c25ee62b17b7642153e110
Viktor <59040542+VityaSchel@users.noreply.github.com>
2022-07-21 20:37:11 +0400

    Seek context

--- src/components/Foreground/Controls/PCControls/SeekBar/Segments.tsx
+++ src/components/Foreground/Controls/PCControls/SeekBar/Segments.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { SeekContext } from '/components/Foreground/Controls/PCControls/SeekBar'
 import { PlayerContext, PlayerProps } from '/components/player'
 import { useAppSelector } from '/store/hooks'
 import { selectPlaysourceVideoDuration } from '/store/slices/playsource'
+import cx from 'classnames'
+
+type Segments = { width: number, startingPosition: number }[]
 
 export default function Segments(props: PlayerProps) {
   const player = React.useContext(PlayerContext)
-  const [segments, setSegments] = React.useState<{ width: number }[]>([])
+  const [segments, setSegments] = React.useState<Segments>([])
   const playsourceDuration = useAppSelector(selectPlaysourceVideoDuration)
 
   React.useEffect(() => {
@@ -17,11 +21,12 @@ export default function Segments(props: PlayerProps) {
 
     const timeToPercent = (secondsFromStart: number) => secondsFromStart / playsourceDuration * 100
 
-    const newSegments: { width: number }[] = []
+    const newSegments: Segments = []
     if (props.segments[0].timeStart !== 0) {
       const nextSegment = props.segments[0]
       newSegments.push({
-        width: nextSegment ? timeToPercent(nextSegment.timeStart) : 100
+        width: nextSegment ? timeToPercent(nextSegment.timeStart) : 100,
+        startingPosition: 0
       })
     }
 
@@ -31,7 +36,8 @@ export default function Segments(props: PlayerProps) {
       newSegments.push({
         width: nextSegment
           ? timeToPercent(nextSegment.timeStart) - filledSpace
-          : 100 - filledSpace
+          : 100 - filledSpace,
+        startingPosition: timeToPercent(props.segments[i].timeStart)
       })
     }
 
@@ -45,19 +51,43 @@ export default function Segments(props: PlayerProps) {
           ? segments.map((segment, i) => (
             <Segment
               width={segment.width}
+              startingPosition={segment.startingPosition}
               key={i}
             />
           ))
-          : <Segment width={100} />
+          : <Segment
+            width={100}
+            startingPosition={0}
+          />
       }
     </div>
   )
 }
 
-function Segment(props: { width: number }) {
+interface SegmentProps {
+  width: number
+  /** Segment starting position in % */
+  startingPosition: number
+}
+function Segment(props: SegmentProps) {
+  const seekContext = React.useContext(SeekContext)
+
+  const endingPosition = props.startingPosition + props.width
+  const bar = (position: number): string => {
+    if (position > endingPosition) return '100%'
+    else {
+      const relativePos = position - props.startingPosition
+      const relativeWidth = relativePos / props.width * 100
+      return `${Math.max(Math.min(100, relativeWidth), 0)}%`
+    }
+  }
+
   return (
     <div className={styles.segment} style={{ width: `${props.width}%` }}>
-
+      <div 
+        className={cx(styles.progressBar, styles.selection)} 
+        style={{ width: bar(seekContext.selectionPosition) }} 
+      />
     </div>
   )
 }
--- src/components/Foreground/Controls/PCControls/SeekBar/index.tsx
+++ src/components/Foreground/Controls/PCControls/SeekBar/index.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import { PlayerProps } from '/components/player'
+import { PlayerContext, PlayerProps } from '/components/player'
 import Segments from './Segments'
-import ProgressBars from './ProgressBars'
 import cx from 'classnames'
+
+interface SeekContext {
+  /** Selection position in % */
+  selectionPosition: number
+}
+export const SeekContext = React.createContext<SeekContext>({ selectionPosition: 0 })
 
 export default function SeekBar(props: PlayerProps) {
   const [hover, setHover] = React.useState(false)
+  const [selectionPosition, setSelectionPosition] = React.useState<number>(0)
+  const seekBarContainerRef = React.useRef<HTMLDivElement>(null)
+  const playerContext = React.useContext(PlayerContext)
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if(!seekBarContainerRef.current || !playerContext) return
+
+    const pageX = seekBarContainerRef.current.getBoundingClientRect().left + window.scrollX
+    const position = (e.pageX - pageX) / seekBarContainerRef.current.offsetWidth
+    setSelectionPosition(position * 100)
+  }
 
   return (
-    <div 
-      className={cx(styles.seekBarContainer, { [styles.hover]: hover })}
-      // onMouseEnter={() => setHover(true)}
-      // onMouseOut={() => setHover(false)}
-    >
-      <Segments {...props} />
-      <ProgressBars {...props} />
-    </div>
+    <SeekContext.Provider value={{ selectionPosition }}>
+      <div 
+        className={cx(styles.seekBarContainer, { [styles.hover]: hover })}
+        onMouseMove={handleMouseMove}
+        // onMouseOut={() => setSelectionPosition(0)}
+        ref={seekBarContainerRef}
+      >
+        <Segments {...props} />
+      </div>
+    </SeekContext.Provider>
   )
 }
