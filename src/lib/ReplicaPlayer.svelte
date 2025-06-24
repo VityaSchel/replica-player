80c551d14de640fa8d667c6ead02ef3faa213f6d
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-24 22:16:49 +0400

    Add header

--- src/lib/ReplicaPlayer.svelte
+++ src/lib/ReplicaPlayer.svelte
@@ -21,17 +21,30 @@
 	} & Pick<import('svelte/elements').SvelteHTMLElements['video'], 'crossorigin'> = $props()
 </script>
 
-<!-- svelte-ignore a11y_media_has_caption -->
-<video {src} controls class="w-full h-full bg-black" {crossorigin}>
-	{#if captions}
-		{#each captions as track (track.url)}
-			<track kind="captions" src={track.url} label={track.label} srclang={track.lang} />
-		{/each}
-	{/if}
+<div class="w-full h-full relative">
+	<div class="h-16 w-full flex items-center absolute left-0 top-0 z-[1]">
+		<div class="p-3">
+			<a href="/" class="rounded-full m-[5px] aspect-square w-10 h-10 block">
+				<img
+					src=""
+					alt=""
+					class="bg-neutral-500/50 w-full h-full rounded-full border-0 outline-0"
+				/>
+			</a>
+		</div>
+	</div>
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video {src} controls class="w-full h-full bg-black" {crossorigin}>
+		{#if captions}
+			{#each captions as track (track.url)}
+				<track kind="captions" src={track.url} label={track.label} srclang={track.lang} />
+			{/each}
+		{/if}
 
-	{#if subtitles}
-		{#each subtitles as track (track.url)}
-			<track kind="subtitles" src={track.url} label={track.label} srclang={track.lang} />
-		{/each}
-	{/if}
-</video>
+		{#if subtitles}
+			{#each subtitles as track (track.url)}
+				<track kind="subtitles" src={track.url} label={track.label} srclang={track.lang} />
+			{/each}
+		{/if}
+	</video>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/i18n/' }),tailwindcss(), sveltekit()]
 })
