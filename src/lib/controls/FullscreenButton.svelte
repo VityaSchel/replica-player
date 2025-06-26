b765f7e63fc29b6427de9dcbd6751fb4a5dc247e
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-27 00:57:46 +0400

    Add exit fullscreen button

--- src/lib/controls/FullscreenButton.svelte
+++ src/lib/controls/FullscreenButton.svelte
@@ -11,6 +11,7 @@
 			fullscreen = document.fullscreenElement !== null
 		}
 		document.addEventListener('fullscreenchange', onFullscreenChange)
+		onFullscreenChange()
 		return () => {
 			document.removeEventListener('fullscreenchange', onFullscreenChange)
 		}
@@ -34,18 +35,33 @@
 	aria-pressed={fullscreen}
 >
 	<svg version="1.1" viewBox="0 0 36 36" width="40" height="40">
-		<g class="tl">
-			<path fill="currentColor" d="m 10,16 2,0 0,-4 4,0 0,-2 L 10,10 l 0,6 0,0 z"></path>
-		</g>
-		<g class="tr">
-			<path fill="currentColor" d="m 20,10 0,2 4,0 0,4 2,0 L 26,10 l -6,0 0,0 z"></path>
-		</g>
-		<g class="br">
-			<path fill="currentColor" d="m 24,24 -4,0 0,2 L 26,26 l 0,-6 -2,0 0,4 0,0 z"></path>
-		</g>
-		<g class="bl">
-			<path fill="currentColor" d="M 12,20 10,20 10,26 l 6,0 0,-2 -4,0 0,-4 0,0 z"></path>
-		</g>
+		{#if fullscreen}
+			<g class="br">
+				<path fill="currentColor" d="m 14,14 -4,0 0,2 6,0 0,-6 -2,0 0,4 0,0 z"></path>
+			</g>
+			<g class="bl">
+				<path fill="currentColor" d="m 22,14 0,-4 -2,0 0,6 6,0 0,-2 -4,0 0,0 z"></path>
+			</g>
+			<g class="tl">
+				<path fill="currentColor" d="m 20,26 2,0 0,-4 4,0 0,-2 -6,0 0,6 0,0 z"></path>
+			</g>
+			<g class="tr">
+				<path fill="currentColor" d="m 10,22 4,0 0,4 2,0 0,-6 -6,0 0,2 0,0 z"></path>
+			</g>
+		{:else}
+			<g class="tl">
+				<path fill="currentColor" d="m 10,16 2,0 0,-4 4,0 0,-2 L 10,10 l 0,6 0,0 z"></path>
+			</g>
+			<g class="tr">
+				<path fill="currentColor" d="m 20,10 0,2 4,0 0,4 2,0 L 26,10 l -6,0 0,0 z"></path>
+			</g>
+			<g class="br">
+				<path fill="currentColor" d="m 24,24 -4,0 0,2 L 26,26 l 0,-6 -2,0 0,4 0,0 z"></path>
+			</g>
+			<g class="bl">
+				<path fill="currentColor" d="M 12,20 10,20 10,26 l 6,0 0,-2 -4,0 0,-4 0,0 z"></path>
+			</g>
+		{/if}
 	</svg>
 </button>
 
@@ -76,7 +92,7 @@
 				transform: translate(0px, 0px);
 			}
 			50% {
-				transform: translate(unquote('#{nth($translate, 1)}'), unquote('#{nth($translate, 2)}'));
+				transform: translate(#{nth($translate, 1)}, #{nth($translate, 2)});
 			}
 		}
 	}
